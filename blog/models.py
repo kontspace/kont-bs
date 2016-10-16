1564c55f21f9025614db946379dc90246e635a61
zhengxiaowai <h1x2y3awalm@gmail.com>
2016-10-16 23:51:31 +0800

    #

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import uuid
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    uuid = models.UUIDField('文章uuid', default=uuid.uuid4, editable=False)
+    title = models.CharField(u'文章标题', max_length=200, help_text=u'文章标题')
+    created_at = models.DateTimeField('创建日期', auto_now_add=True)
+    updated_at = models.DateTimeField('最后修改日期', auto_now=True)
+    post_file = models.FileField(u'文件', upload_to='upload')
+
+    def __unicode__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(u'类别名字', max_length=200)
+    posts = models.ForeignKey(Post, related_name='category', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'post_list.html'
