68fcb98fcdb611248e685af0ef99c1135759d1fe
zhengxiaowai <h1x2y3awalm@gmail.com>
2016-12-11 20:56:57 +0800

    add filter category

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
+
+from collections import OrderedDict as _OrderedDict
 
 import info_config
+
+
+def get_category_numbers():
+    _categories = Category.objects.all().values_list('name')
+    categories = map(lambda x: x[0], _categories)
+
+    categories_numbers = _OrderedDict({'all': Post.objects.all().count()})
+    categories_numbers.update({x: Post.objects.filter(category__name=x).count() for x in categories})
+    return categories_numbers
 
 
 class PostListView(ListView):
@@ -9,9 +20,21 @@ class PostListView(ListView):
     context_object_name = 'posts'
     template_name = 'post_list.html'
 
+    def get_default_context(self, **kwargs):
+        return super(PostListView, self).get_context_data(**kwargs)
+
+    def get_queryset(self):
+        query_values = self.request.GET.get('category', 'all')
+
+        category = Post.objects.all() \
+            if query_values == 'all' \
+            else Post.objects.filter(category__name=query_values.title())
+        return category
+
     def get_context_data(self, **kwargs):
-        context = super(PostListView, self).get_context_data(**kwargs)
+        context = self.get_default_context(**kwargs)
         context['info'] = info_config
+        context['categories'] = get_category_numbers()
         return context
 
 
